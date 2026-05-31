1630d5ef5a5a33b24c031d79215663d497a88fdf
zedzhen <59135268+zedzhen@users.noreply.github.com>
2026-05-31 18:49:59 +0300

    fix

--- src/sphinx_refs_alias/ext.py
+++ src/sphinx_refs_alias/ext.py
@@ -1,43 +1,39 @@
 from typing import TypeAlias
 
-from docutils.nodes import Text, TextElement
+from docutils.nodes import Text, document
 from sphinx.addnodes import pending_xref
 from sphinx.application import Sphinx
-from sphinx.environment import BuildEnvironment
-from sphinx.ext.intersphinx import missing_reference
 
 from sphinx_refs_alias import __version__
 
 T_REFS_ALIAS: TypeAlias = dict[str, str | tuple[str, str | None]]
 
 
-def resolve_reference_aliases(app: Sphinx, env: BuildEnvironment, node: pending_xref, contnode: TextElement):
+def resolve_reference_aliases(app: Sphinx, doctree: document):
     """based on https://stackoverflow.com/a/62301461/18269227"""
     reference_aliases: T_REFS_ALIAS = app.config["reference_aliases"]
+    for node in doctree.findall(condition=pending_xref):
+        alias = node.get("reftarget")
+        if (value := reference_aliases.get(alias)) is not None:
+            new_ref: str
+            new_text: str | None
+            if isinstance(value, str):
+                new_ref = value
+                new_text = None
+            elif isinstance(value, tuple) and len(value) == 2:
+                new_ref, new_text = reference_aliases[alias]
+            else:
+                raise ValueError("The values in 'reference_aliases' must be a str or a tuple[str, str | None]")
 
-    alias = node.get("reftarget")
-    if (value := reference_aliases.get(alias)) is not None:
-        new_ref: str
-        new_text: str | None
-        if isinstance(value, str):
-            new_ref = value
-            new_text = None
-        elif isinstance(value, tuple) and len(value) == 2:
-            new_ref, new_text = reference_aliases[alias]
-        else:
-            raise ValueError("The values in 'reference_aliases' must be a str or a tuple[str, str | None]")
-
-        node["reftarget"] = new_ref
-        if new_text is not None:
-            text_node = next(iter(node.findall(lambda n: n.tagname == "#text")))
-            text_node.parent.replace(text_node, Text(new_text))
-        return missing_reference(app, env, node, contnode)
-    return None
+            node["reftarget"] = new_ref
+            if new_text is not None:
+                text_node = next(iter(node.findall(lambda n: n.tagname == "#text")))
+                text_node.parent.replace(text_node, Text(new_text))
 
 
 def setup(app: Sphinx) -> dict:
-    app.add_config_value("reference_aliases", dict, "env", T_REFS_ALIAS)
-    app.connect("missing-reference", resolve_reference_aliases)
+    app.add_config_value("reference_aliases", lambda config: dict(), "env", T_REFS_ALIAS)
+    app.connect("doctree-read", resolve_reference_aliases)
     return {
         "version": __version__,
         "parallel_read_safe": True,
